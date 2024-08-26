6992f3e8ba224c7db5136fa3c73e8313b73f21f4
luizfmarques <luiz_icb@hotmail.com>
2024-08-26 14:57:25 -0300

    create repositories

---
+++ src/main/java/com/pagamentos/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pagamentos.repositories;
+
+import com.pagamentos.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+}
