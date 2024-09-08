c0c1aac729d1e12be87998ea36f87f41c80d02f6
luizfmarques <luiz_icb@hotmail.com>
2024-09-08 15:39:11 -0300

    add swagger documentation

--- src/main/java/com/pagamentos/controllers/TransactionController.java
+++ src/main/java/com/pagamentos/controllers/TransactionController.java
@@ -3,6 +3,11 @@ package com.pagamentos.controllers;
 import com.pagamentos.dtos.TransactionDTO;
 import com.pagamentos.entities.Transaction;
 import com.pagamentos.services.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +23,17 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @Operation(summary = "Cria uma nova transação", description = "Cria uma nova transação com os dados fornecidos e retorna a transação criada")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transação criada com sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Transaction.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Dados da transação inválidos", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
         Transaction newTransaction = this.transactionService.createTransaction(transaction);
         return new ResponseEntity<>(newTransaction, HttpStatus.OK);
     }
-
 }
--- src/main/java/com/pagamentos/controllers/UserController.java
+++ src/main/java/com/pagamentos/controllers/UserController.java
@@ -3,6 +3,11 @@ package com.pagamentos.controllers;
 import com.pagamentos.dtos.UserDTO;
 import com.pagamentos.entities.User;
 import com.pagamentos.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,21 +15,35 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController()
+@RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    @Operation(summary = "Cria um novo usuário", description = "Cria um novo usuário no sistema e retorna o usuário criado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "Usuário já cadastrado", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
+    })
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody UserDTO user){
+    public ResponseEntity<User> createUser(
+            @RequestBody UserDTO user) {
         User newUser = userService.createUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Busca todos os usuários", description = "Retorna uma lista com todos os usuários cadastrados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna todos os usuários", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
+    })
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers(){
+    public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = this.userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
